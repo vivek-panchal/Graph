579897543556e1f487836b533b5bbcc99228e8ff
vivek <95127627+vivek-panchal@users.noreply.github.com>
2023-01-03 13:36:05 +0530

    Update and rename Leetcode 785. Is Graph Bipartite?.cpp to [DFS + BFS} Leetcode 785. Is Graph Bipartite?.cpp

--- [DFS + BFS} Leetcode 785. Is Graph Bipartite?.cpp
+++ [DFS + BFS} Leetcode 785. Is Graph Bipartite?.cpp
@@ -38,3 +38,40 @@ public:
         return true;
     }
 };
+
+
+// ****************************************DFS**************************************************
+
+class Solution {
+public:
+    bool dfs(int node,int col,vector<vector<int>> &graph,vector<int>&color){
+       color[node]=col;
+
+       for(auto it : graph[node]){
+           if(color[it]==-1){
+               if(dfs(it,!col,graph,color)==false){
+                   return false;
+                }
+               
+            }
+           else if(color[it]==col){
+                   return false;
+                }
+       }
+       return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> color(n,-1);
+
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(dfs(i,0,graph,color)==false){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
